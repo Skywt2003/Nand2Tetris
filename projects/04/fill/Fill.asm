906b26eae20ae19eefce530b261c5035d7a59d1f
skywt2003 <skywt2003@gmail.com>
2022-04-25 22:51:30 +0800

    Complete exercise#4 task#2

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BIGLOOP)
+  @KBD
+  D=M
+  @NPRESSED
+  D;JEQ
+  @PRESSED
+  0;JMP
+
+(PRESSED)
+  @i
+  M=0
+  (LOOPP)
+    @8192
+    D=A
+    @i
+    D=M-D // D=M-16 is not valid, since '=' can only deal with -1,0,1
+    @BIGLOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    M=M+1
+    A=D
+    D=0
+    M=!D
+    @LOOPP
+    0;JMP
+
+(NPRESSED)
+  @i
+  M=0
+  (LOOPN)
+    @8192
+    D=A
+    @i
+    D=M-D
+    @BIGLOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    M=M+1
+    A=D
+    M=0
+    @LOOPN
+    0;JMP
